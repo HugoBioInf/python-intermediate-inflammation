2814fde3d0019e1d5209561fe716b6d470fbb288
HugoBioInf <naarutsu@outlook.fr>
2024-05-24 13:51:54 +0200

    tqt

--- tests/test_models.py
+++ tests/test_models.py
@@ -6,7 +6,7 @@ import pytest
 
 
 @pytest.mark.parametrize(
-          "test, expected"
+          "test, expected",
           [
         ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
         ([ [1, 2], [3, 4], [5, 6] ], [3, 4]),
@@ -17,7 +17,7 @@ def test_daily_mean(test, expected):
      npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
 
 @pytest.mark.parametrize(
-          "test, expected"
+          "test, expected",
           [
         ([ [0, 0, 0], [0, 0, 0], [0, 0, 0] ], [0, 0, 0]),
         ([ [4, 2, 5], [1, 6, 2], [4, 1, 9] ], [4, 6, 9]),
