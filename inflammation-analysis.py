e88ad046d7224be7f343cc354f221b7f8a79fccd
HugoBioInf <naarutsu@outlook.fr>
2024-05-24 10:52:11 +0200

    Code style fixes

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -14,14 +14,17 @@ def main(args):
     - passing data between models and views
     """
     InFiles = args.infiles
-    if not isinstance(nFilesI, list):
+    if not isinstance(InFiles, list):
         InFiles = [args.infiles]
 
 
     for filename in InFiles:
         inflammation_data = models.load_csv(filename)
 
-        view_data = {'average': models.daily_mean(inflammation_data), 'max': models.daily_max(inflammation_data), 'min': models.daily_min(inflammation_data)}
+        view_data = {'average': models.daily_mean(inflammation_data),
+                     'max': models.daily_max(inflammation_data), 
+                     'min': models.daily_min(inflammation_data)
+                     }
 
         views.visualize(view_data)
 
